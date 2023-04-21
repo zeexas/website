97f5d46e656a8913458b5069e1f7959177179da7
Valery Li <lee_hobb@rambler.ru>
2023-04-21 16:11:22 +0600

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,45 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import classes from './Header.module.css';
+import Sun from '../assets/Sun.svg';
+import Moon from '../assets/Moon.svg';
+
 function Header() {
+  const [mode, setMode] = useState('light');
+  const [lang, setLang] = useState('en');
+  const handleChangeMode = () => {
+    mode === 'dark' ? setMode('light') : setMode('dark');
+  };
+  const handleChangeLang = () => {
+    lang === 'en' ? setLang('ru') : setLang('en');
+  };
+
   return (
-    <header>
-      <div>Home icon</div>
-      <div>
-        <ul>
-          <li>RU</li>
-          <li>Mode</li>
-          <li>Hamburger</li>
-        </ul>
+    <header className="bg-slate-300 h-auto flex flex-row flex-nowrap justify-between items-center px-32 py-2 mt-16">
+      <Link to="/">Home icon</Link>
+      <div className="flex flex-row flex-nowrap gap-8 items-center">
+        <div
+          onClick={handleChangeLang}
+          className="uppercase cursor-pointer font-semibold"
+        >
+          {lang}
+        </div>
+        <img
+          onClick={handleChangeMode}
+          src={mode === 'dark' ? Sun : Moon}
+          alt="switch dark/light mode"
+          className="cursor-pointer"
+        />
+        <div className="w-[3rem]">
+          <input id="burgerHandler" type="checkbox" className='hidden' />
+          <label htmlFor="burgerHandler" className={classes.burger}>
+            <div className='flex flex-row flex-nowrap gap-1'>
+              <div className='bg-green-500 w-[50%]'></div>
+              <div className='bg-green-500 w-[50%]'></div>
+            </div>
+          </label>
+        </div>
       </div>
     </header>
   );
