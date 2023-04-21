766fb6db2b944ab180d741d339bfcebfaaecda67
Valery Li <lee_hobb@rambler.ru>
2023-04-21 11:10:33 +0600

    add basic routing

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <div>Home icon</div>
+      <div>
+        <ul>
+          <li>RU</li>
+          <li>Mode</li>
+          <li>Hamburger</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Work.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WorkPage() {
+  return (
+    <div>Work Page</div>
+  )
+}
+
+export default WorkPage
