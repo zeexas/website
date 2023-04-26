60c2a0ce4babc888bb5efd59f9bf9c900d56e8e7
Valery Li <lee_hobb@rambler.ru>
2023-04-26 23:10:34 +0600

    refactor work page, add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        hoverItem: 'hover_item 0.35s ease-in-out forwards',
+        freeItem: 'free_item 0.35s ease-out forwards',
+        showWorkImage: 'show_work_img 0.5s linear forwards',
+        hideWorkImage: 'hide_work_img 0.5s linear forwards',
+      },
+      keyframes: {
+        hover_item: {
+          '0%': { transform: 'translateX(-1.5rem)' },
+          '50%': { transform: 'translateX(0.25rem)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        free_item: {
+          '0%': { transform: 'translateX(0)' },
+          '50%': { transform: 'translateX(-1.75rem)' },
+          '100%': { transform: 'translateX(-1.5rem)' },
+        },
+        show_work_img: {
+          '0%': { opacity: 0, visibility: 'hidden' },
+          '30%': { opacity: 1, visibility: 'visible' },
+          '100%': { opacity: 1, visibility: 'visible' },
+        },
+        hide_work_img: {
+          '0%': { opacity: 1, visibility: 'visible' },
+          '30%': { opacity: 0, visibility: 'hidden' },
+          '100%': { opacity: 0, visibility: 'hidden' },
+        },
+      },
+    },
   },
   plugins: [],
 };
